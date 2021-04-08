276945e2f2606cf303d2f6d80cdd21b7760e3803
Bruno de Oliveira Macedo <odecam@gongolo.onde>
2021-04-08 09:21:36 -0300

    emerson e bruno fizeram algumas regras semanticas

--- sintatico.py
+++ sintatico.py
@@ -107,12 +107,20 @@ def p_campos(p):
     "campos : ID ':' tipo_dado lista_campos"
     
     tabela_sim.update({p[1]: {'type':p[3]}})
+
+    p[0] = 1 + p[4]
     
     print("campos reconhecido")
 
 def p_lista_campos(p):
     '''lista_campos : ';' campos
                     | empty ''' #empty rule
+
+    if (p[1] == None):
+        p[0] = 0
+    else:
+        p[0] = p[2]
+        
     print("lista_campos reconhecido")
     
 def p_tipo_dado(p):
@@ -132,17 +140,24 @@ def p_funcao(p):
     '''funcao : FUNCTION new_scope nome_funcao bloco_funcao'''
     print("Escopo da função: ", p[3][0], "\n", tabela_sim)
     del_scope_pop()
-    tabela_sim.update({p[3][0] : {"type" : "function", "return_type" : p[3][1]}})
+    tabela_sim.update({p[3][0] : {"type" : "function", "return_type" : p[3][1], "n_param" : p[3][2]}})
     print('Funcao reconhecido')
 
 def p_nome_funcao(p):
     '''nome_funcao : ID param_func ':' tipo_dado'''
-    p[0] = (p[1], p[4])
+    tabela_sim.update({'result' : {'type' : p[4]}, 'nome' : p[1]})
+    p[0] = (p[1], p[4], p[2])
     print('Nome_funcao reconhecido')
 
 def p_param_func(p):
     '''param_func : '(' campos ')'
-                  |  '''
+                  | empty '''
+
+    if (p[1] == None):
+        p[0] = 0
+    else:
+        p[0] = p[2]
+    
     print('param_func reconhecido')
 
 def p_bloco_funcao(p):
@@ -165,7 +180,22 @@ def p_comando(p):
                | IF exp_logica THEN bloco else
                | WRITE const_valor
                | READ ID nome'''
-    print('comando reconhecido')
+
+    if (p[1] not in ['while', 'if', 'write', 'read']):
+        if (p[1] in tabela_sim):
+            print( "\n\n Variavel declarada antes do \n\n")
+            print( p[4] )
+            for i in p[4]:
+                if (tabela_sim[p[1]]['type'] == 'real'):
+                    if (i not in ['real', 'integer']):
+                        print("Erro semântico tipo esperado não encontrado da variável: ", p[1])
+                    else:
+                        continue
+                if (tabela_sim[p[1]]['type'] != i):
+                    print("Erro semântico tipo esperado não encontrado da variável: ", p[1])
+        else:
+            print( "\n\n não foi: ", p[1], " \n\n" )
+
 
 def p_else(p):
     '''else : ELSE bloco
@@ -191,15 +221,41 @@ def p_exp_logica_aux(p):
 
 def p_exp_mat(p):
     '''exp_mat : parametro exp_mat_aux'''
-    print('exp_mat reconhecido')
+    p[0] = p[2] + [p[1]]
+    print('\n\nexp_mat reconhecido: ', p[0], '\n\n')
 
 def p_exp_mat_aux(p):
     '''exp_mat_aux : OP_MAT exp_mat
                    | empty''' # empty rule
+    if (p[1] != None):
+        p[0] = p[2]
+    else:
+        p[0] = []
 
 def p_parametro(p):
     '''parametro : ID nome
                  | NUMERO'''
+    
+    global tabela_sim
+
+    # Se der erro é porque é numero
+    # Estamos retornando o tipo pra faze a segunda regra semantica
+    try:
+        p[2]
+        if (tabela_sim[p[1]]['type'] == 'function'):
+            p[0] = tabela_sim[p[1]]['return_type']
+            print("\n\nRetornou função: ", p[0], "\n\n")
+        else:
+            p[0] = tabela_sim[p[1]]['type']
+            print("\n\nRetornou ", p[0], "\n\n")
+    except:
+         if (p[1].find('.') != -1):
+             p[0] = 'real'
+             print("\n\n Retornou float \n\n")
+         else:
+             p[0] = 'integer'
+             print("\n\n Retornou int \n\n")
+        
     print('parametro reconhecido')
 
 def p_op_logico(p):
@@ -315,7 +371,7 @@ end
 begin
 A:=TAM + 20;
 B := fatorial(A);
-ppC := exp(A,B);
+C := exp(A,B);
 D := media(E);
 F := lerDados()
 end
